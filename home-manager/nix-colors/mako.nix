45bc69b8a767f437474d80829195947f298537c4
SphericalPB <xdmaxgamer360@gmail.com>
2024-03-16 21:50:05 +0800

    2024-03-16_13:50:05

--- home-manager/nix-colors/mako.nix
+++ home-manager/nix-colors/mako.nix
@@ -2,12 +2,11 @@
   pkgs,
   config,
   ...
-}:
-
-{
+}: {
   services.mako = {
     backgroundColor = "#${config.colorScheme.palette.base00}";
     textColor = "#${config.colorScheme.palette.base05}";
     borderColor = "#${config.colorScheme.palette.base0E}";
     progressColor = "#${config.colorScheme.palette.base0B}";
+  };
 }
