2206fe753fd760a46b45523f11c5e58b40edd8a6
SphericalPB <xdmaxgamer360@gmail.com>
2024-03-16 21:24:40 +0800

    2024-03-16_13:24:40

---
+++ home-manager/nix-colors/default.nix
@@ -0,0 +1,12 @@
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  imports = [
+    inputs.nix-colors.homeManagerModules.default
+    ./kitty.nix
+  ];
+
+  colorScheme = inputs.nix-colors.colorSchemes.catppuccin-macchiato;
+}
