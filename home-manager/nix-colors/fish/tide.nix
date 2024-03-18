0c0a89eed838b54cbde985af33bd83c22bf4f62d
SphericalPB <xdmaxgamer360@gmail.com>
2024-03-18 20:53:25 +0800

    2024-03-18_12:53:25

--- home-manager/nix-colors/fish/tide.nix
+++ home-manager/nix-colors/fish/tide.nix
@@ -9,14 +9,15 @@
       # set tide_color_frame_and_connection
       set tide_color_separator_same_color ${config.colorScheme.palette.base04}
       ## OS
-      set tide_os_bg_color ${config.colorScheme.palette.base03}
+      set tide_os_bg_color ${config.colorScheme.palette.base02}
       set tide_os_color ${config.colorScheme.palette.base05}
       ## Directory
+      set tide_pwd_bg_color ${config.colorScheme.palette.base03}
       set tide_pwd_color_anchors ${config.colorScheme.palette.base0C}
       set tide_pwd_color_dirs ${config.colorScheme.palette.base0C}
       ## Time
-      set tide_time_bg_color ${config.colorScheme.palette.base0F}
-      set tide_time_color ${config.colorScheme.palette.base02}
+      set tide_time_bg_color ${config.colorScheme.palette.base02}
+      set tide_time_color ${config.colorScheme.palette.base0A}
     '';
   };
 }
