1c2c874dd17be0eb8e1aa4608e6f608595fb848d
SphericalPB <xdmaxgamer360@gmail.com>
2024-03-21 17:05:03 +0800

    2024-03-21_09:05:03

--- home-manager/applications/hypr/winrules.nix
+++ home-manager/applications/hypr/winrules.nix
@@ -11,15 +11,15 @@
       # Workspace Rules
       workspace = [
       ];
-      extraConfig = ''
-        Gromit-MPX Fix (https://www.reddit.com/r/hyprland/comments/18kutkk)
-             workspace = special:gromit, gapsin:0, gapsout:0, on-created-empty: gromit-mpx -a
-             windowrule = noblur, ^(Gromit-mpx)$
-             windowrule = opacity 1 override, 1 override, ^(Gromit-mpx)$
-             windowrule = noshadow, ^(Gromit-mpx)$
-             windowrule = suppressevent, fullscreen, ^(Gromit-mpx)$
-             windowrule = size 100% 100%, ^(Gromit-mpx)$
-      '';
     };
+    extraConfig = ''
+      Gromit-MPX Fix (https://www.reddit.com/r/hyprland/comments/18kutkk)
+           workspace = special:gromit, gapsin:0, gapsout:0, on-created-empty: gromit-mpx -a
+           windowrule = noblur, ^(Gromit-mpx)$
+           windowrule = opacity 1 override, 1 override, ^(Gromit-mpx)$
+           windowrule = noshadow, ^(Gromit-mpx)$
+           windowrule = suppressevent, fullscreen, ^(Gromit-mpx)$
+           windowrule = size 100% 100%, ^(Gromit-mpx)$
+    '';
   };
 }
