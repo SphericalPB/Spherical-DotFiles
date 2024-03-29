dee4cde4a97fd4e05872c039200d67695d764521
SphericalPB <xdmaxgamer360@gmail.com>
2024-03-29 18:39:13 +0800

    2024-03-29_10:39:13

--- home-manager/applications/eww/default.nix
+++ home-manager/applications/eww/default.nix
@@ -1,10 +1,23 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
-  programs.eww = {
-    enable = true;
-    configDir = "~/.config/nix-conf/home-manager/applications/eww/eww";
+{ inputs, lib, config, pkgs, ... }:
+with lib;
+let
+    cfg = config.modules.eww;
+in {
+    options.modules.eww = { enable = mkEnableOption "eww"; };
+
+    config = mkIf cfg.enable {
+        # theres no programs.eww.enable here because eww looks for files in .config
+        # thats why we have all the home.files
+
+        # eww package
+        home.packages = with pkgs; [
+            eww-wayland
+        ];
+
+        # configuration
+        home.file.".config/eww/eww.scss".source = ./eww.scss;
+        home.file.".config/eww/eww.yuck".source = ./eww.yuck;
   };
+ };
 }
+
