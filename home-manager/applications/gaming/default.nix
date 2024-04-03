390dd7bba1e6c89bcd748ad185ec5f68ccd50c43
SphericalPB <xdmaxgamer360@gmail.com>
2024-04-03 20:57:34 +0800

    2024-04-03_12:57:34

--- home-manager/applications/gaming/default.nix
+++ home-manager/applications/gaming/default.nix
@@ -11,7 +11,9 @@
   home.packages = [
     # inputs.nix-gaming.packages.${pkgs.system}.<package>
     pkgs.gamemode
+    inputs.nix-gaming.pakgages.${pkgs.system}.osu-mime
     inputs.nix-gaming.packages.${pkgs.system}.osu-lazer-bin
-    pkgs.grapejuice
+    inputs.nix-gaming.pakgages.${pkgs.system}.osu-stable
+    inputs.nix-gaming.pakgages.${pkgs.system}.roblox-player
   ];
 }
