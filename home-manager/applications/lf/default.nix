3bfba683a3b163ec22c732a6f1f854f0c69f365a
SphericalPB <xdmaxgamer360@gmail.com>
2024-04-07 20:01:20 +0800

    2024-04-07_12:01:20

--- home-manager/applications/lf/default.nix
+++ home-manager/applications/lf/default.nix
@@ -56,16 +56,16 @@
     previewer = {
       keybinding = "p";
       source = pkgs.writeShellScriptBin "pv.sh" ''
-                #!/bin/sh
-        file=$1
+        #!/usr/bin/env bash
+               file=$1
                w=$2
                h=$3
                x=$4
                y=$5
 
                if [[ "$( ${pkgs.file}/bin/file -Lb --mime-type "$file")" =~ ^image ]]; then
-                   ${pkgs.kitty}/bin/kitty +kitten icat --silent --stdin no --transfer-mode file --place "''${w}x''${h}@''${x}x''${y}" "$file" < /dev/null > /dev/tty
-                   exit 1
+               ${pkgs.kitty}/bin/kitty +kitten icat --silent --stdin no --transfer-mode file --place "''${w}x''${h}@''${x}x''${y}" "$file" < /dev/null > /dev/tty
+               exit 1
                fi
 
                ${pkgs.pistol}/bin/pistol "$file"
