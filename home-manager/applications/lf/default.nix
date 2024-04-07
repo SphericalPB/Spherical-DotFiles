ed992090b688a6c5dc938e4091b60afac7fa6b15
SphericalPB <xdmaxgamer360@gmail.com>
2024-04-07 20:24:31 +0800

    2024-04-07_12:24:31

--- home-manager/applications/lf/default.nix
+++ home-manager/applications/lf/default.nix
@@ -55,33 +55,30 @@
 
     previewer = {
       keybinding = "p";
-      source = let 
-      previewer = 
-        pkgs.writeShellScriptBin "pv.sh" ''
-        file=$1
-        w=$2
-        h=$3
-        x=$4
-        y=$5
-        
-        if [[ "$( ${pkgs.file}/bin/file -Lb --mime-type "$file")" =~ ^image ]]; then
-            ${pkgs.kitty}/bin/kitty +kitten icat --silent --stdin no --transfer-mode file --place "''${w}x''${h}@''${x}x''${y}" "$file" < /dev/null > /dev/tty
-            exit 1
-        fi
-        
-        ${pkgs.pistol}/bin/pistol "$file"
-      ''; 
-      in
-      ''${previewer}/bin/pv.sh'';
-    };
-    extraConfig = 
-      let
-        cleaner = pkgs.witeShellScriptBin "clean.sh" ''
-	#!/usr/bin/env bash
-        ${pkgs.kitty}/bin/kitty +kitten icat --clear --stdin no --silent --transfer-mode file < /dev/null > /dev/tty
+      source = let
+        previewer = pkgs.writeShellScriptBin "pv.sh" ''
+          file=$1
+          w=$2
+          h=$3
+          x=$4
+          y=$5
+
+          if [[ "$( ${pkgs.file}/bin/file -Lb --mime-type "$file")" =~ ^image ]]; then
+              ${pkgs.kitty}/bin/kitty +kitten icat --silent --stdin no --transfer-mode file --place "''${w}x''${h}@''${x}x''${y}" "$file" < /dev/null > /dev/tty
+              exit 1
+          fi
+
+          ${pkgs.pistol}/bin/pistol "$file"
         '';
-       in
-       ''
-         set cleaner ${cleaner}/bin/clean.sh
-       '';
+      in ''${previewer}/bin/pv.sh'';
+    };
+    extraConfig = let
+      cleaner = pkgs.witeShellScriptBin "clean.sh" ''
+        #!/usr/bin/env bash
+               ${pkgs.kitty}/bin/kitty +kitten icat --clear --stdin no --silent --transfer-mode file < /dev/null > /dev/tty
+      '';
+    in ''
+      set cleaner ${cleaner}/bin/clean.sh
+    '';
+  };
 }
