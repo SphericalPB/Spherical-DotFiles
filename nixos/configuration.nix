e5c508eedc35e85465b73906e91be1840d53f909
SphericalPB <xdmaxgamer360@gmail.com>
2024-04-16 18:22:06 +0800

    2024-04-16_10:22:06

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -227,8 +227,8 @@
       };
       remotePlay.openFirewall = true;
       dedicatedServer.openFirewall = true;
-      extraCompatPackages = with pkgs; [proton-ge-bin];
-    };
+      #extraCompatPackages = with pkgs; [proton-ge-bin];
+    #};
   };
 
   systemd = {
